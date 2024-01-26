8b0d7c46e5e959c60fc72f2be8ee18db27c75f3b
Alberto Di Biase <asdibiase@uc.cl>
2024-01-26 17:40:35 -0300

    Add support to add words to personal dictionary. Better description of settings.

--- test.js
+++ test.js
@@ -2,14 +2,23 @@
 let text = "This is a test with a miztake";
 
 let url = "https://api.languagetoolplus.com/v2/check";
+let url_2 = url.split("check")[0] + "words/add"
+console.log(url_2);
+
+// let data_obj = {
+//     "text": text,
+//     "language": "auto",
+//     // "disabledRules": "",
+//     // "motherTongue": "",
+//     // "preferredVariants": "",
+//     // "enabledCategories": "",
+//     // "disabledCategories": "",
+// }
+
 let data_obj = {
-    "text": text,
-    "language": "auto",
-    // "disabledRules": "",
-    // "motherTongue": "",
-    // "preferredVariants": "",
-    // "enabledCategories": "",
-    // "disabledCategories": "",
+    word: "mstake",
+    username: "alberto.di.b.o@gmail.com",
+    apiKey: "pit-CUO0ftJ4abbk"
 }
 
 let data = new FormData();
@@ -28,4 +37,4 @@ let options = {
 }
 
 
-const response = fetch(url, options).then(response => response.json()).then(response => console.log(response));
+const response = fetch(url_2, options).then(response => response.json()).then(response => console.log(response));
